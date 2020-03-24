ae28e83a9d20ea270410de52bfb5ef0eb74b9d09
Noa Rotem <noa.rotem@live.biu.ac.il>
2020-03-24 17:06:23 +0200

    first commit

---
+++ FlightSimulatorApp1/IModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlightSimulatorApp1
+{
+    interface IModel : INotifyPropertyChanged
+    {
+        //DASHBOARD-לוח מחוונים
+        void connect(string ip, int port);
+        void disconnect();
+        void start();
+        double indicatedHeadingDeg { set; get; }
+        double gpsIndicatedVerticalSpeed { set; get; }
+        double gpsIndicatedGroundSpeed { set; get; }
+        double airspeedIndicatorIndicatedSpeed { set; get; }
+        double gpsIndicatedAltitude { get; set; }
+        double attitudeIndicatorInternalRollDeg { get; set; }
+        double attitudeIndicatorInternalpitchDeg { get; set; }
+        double altimeterIndicatedAltitude { get; set; }
+        //for joistick- לוח ההגאים
+        double rudder { get; set; }
+        double elevator { get; set; }
+
+        //sliders
+        double aileron { get; set; }
+        double throttle { get; set; }
+    }
+}
